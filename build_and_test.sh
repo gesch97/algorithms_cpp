09d445eba787a2cbb9c71f9a82cfef31da663a4f
Gergo Schonhardt <48925940+gesch97@users.noreply.github.com>
2025-02-21 11:40:49 +0100

    changes in the build process

--- build_and_test.sh
+++ build_and_test.sh
@@ -11,7 +11,7 @@ else
 fi
 
 cd build
-cmake -DCMAKE_BUILD_TYPE=Debug -DLLDB_EXPORT_ALL_SYMBOLS=ON .. || { echo "CMake configuration failed"; cd .. ; exit 1; }
+cmake .. || { echo "CMake configuration failed"; cd .. ; exit 1; }
 make || { echo "Make failed"; cd ..; exit 1; }
 
 ./tests/MyTests
